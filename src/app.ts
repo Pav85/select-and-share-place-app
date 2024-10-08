c9de138234556bcbc0c72e961f9d3a13959d4b5b
Pawel <pawel.werbowy@gmail.com>
2024-10-08 12:07:43 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,20 +13,31 @@ if (!apiKey) {
   console.log("API key successfully loaded");
 }
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   axios
-    .get(
+    .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${apiKey}`
     )
     .then((response) => {
-      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
     })
     .catch((err) => {
+      alert(err.message);
       console.log(err);
     });
 }
